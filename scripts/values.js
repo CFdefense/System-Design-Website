0092657e0844a2d0bbad129e6a0b447a68bd1274
RobsenGelalcha <robsengudeta1@outlook.com>
2024-10-21 21:43:59 -0400

    added type writer effect to the header above hero

--- scripts/values.js
+++ scripts/values.js
@@ -14,3 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+var i = 0;
+var txt = 'What we value makes us different.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".learn").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
