f06a49026f1415b35584256034ed13289bbe589f
RobsenGelalcha <robsengudeta1@outlook.com>
2024-10-22 13:28:49 -0400

    added images, hover-text

---
+++ scripts/clients.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelectorAll('.tab');
+    const tabPanes = document.querySelectorAll('.tab-pane');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', function () {
+           
+            tabs.forEach(tab => tab.classList.remove('active'));
+            tabPanes.forEach(pane => pane.classList.remove('active'));
+
+            
+            tab.classList.add('active');
+            tabPanes[index].classList.add('active');
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelectorAll('.tab');
+    const tabPanes = document.querySelectorAll('.tab-pane');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', function () {
+           
+            tabs.forEach(tab => tab.classList.remove('active'));
+            tabPanes.forEach(pane => pane.classList.remove('active'));
+
+            
+            tab.classList.add('active');
+            tabPanes[index].classList.add('active');
+        });
+    });
+});
+
+
+var i = 0;
+var txt = "We bring our clients' imagination to life!"; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".learn").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
+
